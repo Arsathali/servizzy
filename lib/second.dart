8d3cc008b31f6b4d233f9e775ba072eb27ee34ad
Arsath ali <arsatharsha17@gmail.com>
2024-12-07 20:51:50 +0530

    changes made

---
+++ lib/second.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class secomd extends StatefulWidget {
+  const secomd({super.key});
+
+  @override
+  State<secomd> createState() => _secomdState();
+}
+
+class _secomdState extends State<secomd> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
